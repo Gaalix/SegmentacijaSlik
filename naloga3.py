6478d8c8da79db3bea0684c420f4597daf27bcbf
Gal Borinc <borincgal2003@gmail.com>
2024-03-29 20:36:45 +0100

    kmeans - spremenimo sliko v 2d

--- naloga3.py
+++ naloga3.py
@@ -2,7 +2,8 @@ import cv2 as cv
 
 def kmeans(slika, k=3, iteracije=10):
     '''Izvede segmentacijo slike z uporabo metode k-means.'''
-    pass
+    # Spremenimo obliko slike v 2D
+    slika_2d = slika.reshape((-1, 3))
 
 def meanshift(slika, velikost_okna, dimenzija):
     '''Izvede segmentacijo slike z uporabo metode mean-shift.'''
